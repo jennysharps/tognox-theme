5eedb455dc1ddbf78fa0836545b9a36464ed6578
Jenny Sharps <jsharps85@gmail.com>
2013-07-07 00:01:04 -0400

    Update bullet style & add GA tracking

--- header.php
+++ header.php
@@ -1,39 +1,49 @@
-<!doctype html>  
+<!doctype html>
 
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-	
+
 	<head>
 		<meta charset="utf-8">
-		
+
 		<title><?php wp_title(''); ?></title>
-		
+
 		<!-- Google Chrome Frame for IE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		
+
 		<!-- mobile meta (hooray!) -->
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		
+
 		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=2">
-				
+
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		
+
 		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
-			
+
 		<!-- drop Google Analytics Here -->
 		<!-- end analytics -->
-		
+
 	</head>
-	
+
 	<body <?php body_class(); ?>>
-			
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-42260396-1', 'francescotonini.com');
+          ga('send', 'pageview');
+        </script>
+
                 <header class="header" role="banner">
 
                         <div id="inner-header" class="wrap clearfix">
