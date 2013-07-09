06835df018f76d8d3152114b3087708f9b997094
Jenny Sharps <jsharps85@gmail.com>
2013-07-09 00:46:41 -0400

    Change downloads permalink while in dev

--- library/custom-post-downloads.php
+++ library/custom-post-downloads.php
@@ -2,8 +2,8 @@
 
 
  // let's create the function for the custom type
-function custom_post_Downloads() { 
-	 // creating (registering) the custom type 
+function custom_post_Downloads() {
+	 // creating (registering) the custom type
 	register_post_type( 'downloads', // (http: //codex.wordpress.org/Function_Reference/register_post_type)
 	 	 // let's now add all the options for this post type
 		array('labels' => array(
@@ -16,8 +16,8 @@ function custom_post_Downloads() {
 			'edit_item' => __('Edit Download'), // Edit Display Title
 			'new_item' => __('New Download'), // New Display Title
 			'view_item' => __('View Download'), // View Display Title
-			'search_items' => __('Search Downloads'), // Search Custom Type Title 
-			'not_found' =>  __('Nothing found in the Database.'), // This displays if there are no entries yet 
+			'search_items' => __('Search Downloads'), // Search Custom Type Title
+			'not_found' =>  __('Nothing found in the Database.'), // This displays if there are no entries yet
 			'not_found_in_trash' => __('Nothing found in Trash'), // This displays if there is nothing in the trash
 			'parent_item_colon' => ''
 			), // end of arrays
@@ -27,30 +27,30 @@ function custom_post_Downloads() {
 			'exclude_from_search' => false,
 			'show_ui' => true,
 			'query_var' => true,
-			'menu_position' => 9, // this is what order you want it to appear in on the left hand side menu 
+			'menu_position' => 9, // this is what order you want it to appear in on the left hand side menu
 			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/download.png', // the icon for the custom post type menu
 			'rewrite' => array( 'slug' => 'downloads', 'with_front' => false ),
 			'capability_type' => 'post',
 			'hierarchical' => false,
-			'permalink_epmask' => 'EP_PERMALINK & EP_YEAR', 
-			'has_archive' => 'downloads',
+			'permalink_epmask' => 'EP_PERMALINK & EP_YEAR',
+			'has_archive' => 'downloads-comingsoon',
 			//'register_meta_box_cb' => 'custom_Download_metaboxes',
 			// the next one is important, it tells what's enabled in the post editor
 			'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', 'sticky'),
 			'taxonomies' => array('category', 'post_tag')
 	 	) // end of options
 	); // end of register post type
-	
-	
+
+
 	// this ads your post categories to your custom post type
 	// register_taxonomy_for_object_type('category', 'main_Downloads');
 	// this ads your post tags to your custom post type
 	//register_taxonomy_for_object_type('post_tag', 'main_Downloads');
-	
-} 
+
+}
 // adding the function to the Wordpress init
 add_action( 'init', 'custom_post_Downloads');
-		
+
 /*
 // Define additional "post thumbnails". Relies on MultiPostThumbnails to work
 if (class_exists('MultiPostThumbnails')) {
@@ -63,11 +63,11 @@ if (class_exists('MultiPostThumbnails')) {
         )
     );
 };
-	
+
 function custom_metabox_test_proj($post_ID = NULL) {
 
 	$meta = get_post_meta( $post_ID, '_slider_order', true );
-	
+
   	$content = wp_nonce_field( 'test-Downloads-options', 'myplugin_noncename', true, false );
 
  	$content .= '
@@ -76,7 +76,7 @@ function custom_metabox_test_proj($post_ID = NULL) {
  		<select id="_test_kw_field" name="_test_kw_field">
  			<option>None</option>
  		';
- 	
+
 	 	foreach (range(1,6) as $number) {
 	 		if( $meta == $number ) { $select = ' selected="selected"'; } else { $select = ''; }
 		    $content .= '<option' . $select . ' value="' . $number . '">' . $number . '</option>';
@@ -85,13 +85,13 @@ function custom_metabox_test_proj($post_ID = NULL) {
 		$content .= '
 		</select>
  	</div>';
-		
+
 	return $content;
 }
 
 
 function save_Download_postdata( $post_id ) {
-  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
       return;
 
   if ( !wp_verify_nonce( $_POST['myplugin_noncename'], 'test-Downloads-options' ) )
@@ -108,9 +108,9 @@ add_action( 'save_post', 'save_Download_postdata' );
 
 
 function main_Downloads_admin_css() {
-	global $post_type; 
-	
-	if ( get_post_type() == 'main_Downloads' || $post_type == 'main_Downloads' ) {	
+	global $post_type;
+
+	if ( get_post_type() == 'main_Downloads' || $post_type == 'main_Downloads' ) {
 
 		echo "<link type='text/css' rel='stylesheet' href='" . get_bloginfo('stylesheet_directory') . "/library/css/admin.css' />";
 	}
@@ -120,7 +120,7 @@ add_action('admin_head', 'main_Downloads_admin_css');
 
 function retrieve_Download_slider_images() {
 	global $post;
-	
+
 	$args = array(
 		'meta_key' => '_slider_order',
 		'post_type' => 'main_Downloads',
@@ -132,26 +132,26 @@ function retrieve_Download_slider_images() {
 			)
 		)
 	);
-					
+
 	$slider_query = new WP_Query($args);
 	$attachments = array();
-					
+
 	if( $slider_query->have_posts() ) {
 		while ($slider_query->have_posts()) : $slider_query->the_post();
-		
+
 			$order = get_post_meta( $post->ID, '_slider_order', true);
-			
+
 			$retrieved = MultiPostThumbnails::get_the_post_thumbnail_url('main_Downloads', 'Downloads_slider_image');
-			
-			$attachments[$order] = array( 
-				'img_url'=>$retrieved[0], 
+
+			$attachments[$order] = array(
+				'img_url'=>$retrieved[0],
 				'link'=>get_permalink(),
 				'title'=>get_the_title());
-		
+
 			endwhile;
 		}
 		wp_reset_query();
-					
+
 		asort($attachments, SORT_NUMERIC);
 		return $attachments;
 }*/
