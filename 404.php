69fffb3e5ed20d2868f1e0bd489c4db6f1f5ab38
Jenny Sharps <jsharps85@gmail.com>
2013-07-28 12:38:21 -0400

    Update 404 template

--- 404.php
+++ 404.php
@@ -1,43 +1,38 @@
 <?php get_header(); ?>
-			
+
 			<div id="content">
 
 				<div id="inner-content" class="wrap clearfix">
-			
+
 					<div id="main" class="eightcol first clearfix" role="main">
 
 						<article id="post-not-found" class="hentry clearfix">
-						
+
 							<header class="article-header">
-							
-								<h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
-						
+
+								<h1><?php _e("Sorry, this page doesn't exist!", "bonestheme"); ?></h1>
+
 							</header> <!-- end article header -->
-					
+
 							<section class="entry-content">
-							
-								<p><?php _e("The article you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
-					
+
+								<p><?php _e("The page you were looking for wasn't found, but you can search to find what you're looking for.", "bonestheme"); ?></p>
+
 							</section> <!-- end article section -->
 
 							<section class="search">
-				
+
 							    <p><?php get_search_form(); ?></p>
-				
+
 							</section> <!-- end search section -->
-						
-							<footer class="article-footer">
-							
-							    <p><?php _e("This is the 404.php template.", "bonestheme"); ?></p>
-							
-							</footer> <!-- end article footer -->
-					
+
+
 						</article> <!-- end article -->
-			
+
 					</div> <!-- end #main -->
 
+                                        <?php get_sidebar(); ?>
 				</div> <!-- end #inner-content -->
-    
-			</div> <!-- end #content -->
 
+			</div> <!-- end #content -->
 <?php get_footer(); ?>
