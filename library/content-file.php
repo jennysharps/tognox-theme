dadde4b5829a68b84b2e68717aa728388a2e4619
Jenny Sharps <jennylynnsharps@gmail.com>
2015-05-02 11:22:48 +0100

    Update logic for file url

--- library/content-file.php
+++ library/content-file.php
@@ -1,9 +1,9 @@
 <?php
-	$file_url = wp_get_attachment_url($meta['related-file'][0]);
+	$file_url = isset($meta['related-file'][0])? wp_get_attachment_url($meta['related-file'][0]): null;
 	$button_text = isset($meta['button-text'][0]) ? $meta['button-text'][0] : 'Download File';
 ?>
 <span class="file-wrapper">
-	<?php if(isset($file_url)) { ?>
+	<?php if($file_url) { ?>
 		<a class="button download-link" href="<?php echo $file_url; ?>" target="_blank">
 			<span class="fa-icon fa-icon-button fa-icon-download"></span><?php _e($button_text); ?>
 		</a>
