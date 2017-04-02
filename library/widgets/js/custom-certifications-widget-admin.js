f8f461591a16f1e8717400c70dd0e1a261800838
Jenny Sharps <jennysharps@users.noreply.github.com>
2017-04-02 12:00:47 +0100

    fixes for new wp update cert widget

--- library/widgets/js/custom-certifications-widget-admin.js
+++ library/widgets/js/custom-certifications-widget-admin.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function($) {
     $('.certification-add-button').live('click', function() {
         var parent = $(this).parent();
+        var container = parent.find('.certification-logos-container')
         var template = parent.find('.certificationItemTemplate').html();
-        $(template).insertBefore(this);
+        $(template).appendTo(container);
     })
 
     $('.certification-delete-button').live('click', function() {
