ef515a8b34dba3d1e275ec116ef9dbf6e431dbdb
Jenny Sharps <jennysharps@users.noreply.github.com>
2017-04-02 12:45:37 +0100

    debug

--- library/widgets/js/custom-certifications-widget-admin.js
+++ library/widgets/js/custom-certifications-widget-admin.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     $('.certification-add-button').live('click', function() {
         var parent = $(this).parent();
         debugger;
-        var container = parent.find('.certification-container');
+        var container = parent.find('.certification-logos-container');
         var template = parent.find('.certificationItemTemplate').html();
         $(template).appendTo(container);
     })
